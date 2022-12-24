6d1f2aab2cb1f838567fc740f5e422f4df414f02
Jonyboylovespie <90563990+Jonyboylovespie@users.noreply.github.com>
2022-12-24 15:09:22 -0500

    Add files via upload

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,7 +2,7 @@
 {
     public static class ModHelperData
     {
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
         public const string Name = "OP Legend Of The Night";
         public const string Description =
             "This mod makes the legend of the night portal suck up anything that leaks, and I mean ANYTHING. You can change the price of the upgrade in mod settings.";
--- OPLegendOfTheNight.cs
+++ OPLegendOfTheNight.cs
@@ -1,38 +1,13 @@
 ﻿using Assets.Scripts.Models;
-using Assets.Scripts.Models.GenericBehaviors;
-using Assets.Scripts.Models.Towers;
-using Assets.Scripts.Models.Towers.Behaviors;
 using Assets.Scripts.Models.Towers.Behaviors.Abilities;
-using Assets.Scripts.Models.Towers.Behaviors.Abilities.Behaviors;
-using Assets.Scripts.Models.Towers.Behaviors.Attack;
-using Assets.Scripts.Models.Towers.Behaviors.Attack.Behaviors;
-using Assets.Scripts.Models.Towers.Filters;
 using Assets.Scripts.Models.Towers.Mods;
-using Assets.Scripts.Models.Towers.Projectiles;
-using Assets.Scripts.Models.Towers.Projectiles.Behaviors;
-using Assets.Scripts.Models.Towers.TowerFilters;
-using Assets.Scripts.Models.Towers.Weapons;
-using Assets.Scripts.Models.Towers.Weapons.Behaviors;
-using Assets.Scripts.Models.TowerSets;
-using Assets.Scripts.Unity;
-using Assets.Scripts.Unity.Display;
-using Assets.Scripts.Utils;
 using BTD_Mod_Helper;
-using BTD_Mod_Helper.Api;
-using BTD_Mod_Helper.Api.Display;
 using BTD_Mod_Helper.Api.Enums;
 using BTD_Mod_Helper.Api.Helpers;
 using BTD_Mod_Helper.Api.ModOptions;
-using BTD_Mod_Helper.Api.Towers;
 using BTD_Mod_Helper.Extensions;
-using Harmony;
 using MelonLoader;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using OPLegendOfTheNight;
-using UnhollowerBaseLib;
-using Il2CppSystem.Collections.Generic;
 
 
 [assembly: MelonInfo(typeof(OPLegendOfTheNightMod), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
