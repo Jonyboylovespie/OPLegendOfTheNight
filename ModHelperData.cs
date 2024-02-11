6d6c2714ec35b4849c5a44c0e0a3bff94d6eba83
Jonyboylovespie <90563990+Jonyboylovespie@users.noreply.github.com>
2024-02-11 04:43:20 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -1,15 +1,14 @@
-﻿namespace OPLegendOfTheNight
-{
-    public static class ModHelperData
-    {
-        public const string WorksOnVersion = "36.3";
-        public const string Version = "1.0.2";
-        public const string Name = "OP Legend Of The Night";
-        public const string Description =
-            "This mod makes the legend of the night portal suck up anything that leaks, and I mean ANYTHING. You can change the price of the upgrade in mod settings.";
+namespace OPLegendOfTheNight;
 
-        public const string RepoOwner = "Jonyboylovespie";
-        public const string RepoName = "OPLegendOfTheNight";
-        public const string Icon = "OPLegendOfTheNightIcon.png";
-    }
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "36.3";
+    public const string Version = "1.0.2";
+    public const string Name = "OP Legend Of The Night";
+    public const string Description =
+        "This mod makes the legend of the night portal suck up anything that leaks, and I mean ANYTHING. You can change the price of the upgrade in mod settings.";
+
+    public const string RepoOwner = "Jonyboylovespie";
+    public const string RepoName = "OPLegendOfTheNight";
+    public const string Icon = "OPLegendOfTheNightIcon.png";
 }
