9ab0ed49b9b320dacc61ea44cc3daaec8c901c8a
Jonyboylovespie <90563990+Jonyboylovespie@users.noreply.github.com>
2024-02-11 04:46:34 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,12 +2,11 @@ namespace OPLegendOfTheNight;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "41.1";
-    public const string Version = "0.1.0"; //0.0.1 is a small bug fix, 0.1.0 is a balance change and 1.0.0 is release or big update
-    public const string Name = "FirstMod";
-    public const string Description = "This is my first ever BTD6 Mod, Called better Monkey Ace, It has 2 upgrades on all paths, however, the mod is still in alpha and all paths will be finished to Teir 5 and even a paragon. Go to the github page for more info.";
-
-    public const string RepoOwner = "Jonyboylovespie";
+    public const string WorksOnVersion = "34.3";
+    public const string WorksOnVersion = "36.3";
+    public const string Version = "1.0.2";
+    public const string Name = "OP Legend Of The Night";
+    public const string Description = "This mod makes the legend of the night portal suck up anything that leaks, and I mean ANYTHING. You can change the price of the upgrade in mod settings.";
     public const string RepoName = "OPLegendOfTheNight";
     public const string Icon = "OPLegendOfTheNightIcon.png";
 }
